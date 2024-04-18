37904babfb54440e0edd056c219d534f580b686c
Heryakos Meles <heryakosmeles@gmail.com>
2024-04-18 17:27:56 +0300

    color mode switch add and adding A new navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/square-hm-logo-design-vector-isolated-white-background-286936945.jpg";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack padding="10px">
       <Image src={logo} boxSize="60px"></Image>
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
